a2591fba2b0a4f8a9f0329e5bd2715fd558c00db
petardjorovic <perodjorovic@gmail.com>
2026-01-30 21:29:06 +0100

    implement user route & controller

--- identity-service/src/routes/identity-servise.route.ts
+++ identity-service/src/routes/identity-servise.route.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { userRegistration } from "../controllers/identity.controller.js";
+import {
+  userLogin,
+  userRegistration,
+} from "../controllers/identity.controller.js";
 
 const router = Router();
 
 router.post("/register", userRegistration);
+router.post("/login", userLogin);
 
 export default router;
