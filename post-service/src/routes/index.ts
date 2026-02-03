33c2cd5ad917cc062346feed559ce884aff2759b
petardjorovic <perodjorovic@gmail.com>
2026-02-03 21:37:19 +0100

    add controllers for posts routes

--- post-service/src/routes/index.ts
+++ post-service/src/routes/index.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { createPost } from "../controllers/post.controller.js";
+import {
+  createPost,
+  deletePost,
+  getPost,
+  getPosts,
+} from "../controllers/post.controller.js";
 
 const postRouter = Router();
 
 postRouter.use(authMiddleware);
 
+// prefix /posts
+
+postRouter.get("/", getPosts);
+postRouter.get("/:id", getPost);
 postRouter.post("/", createPost);
+postRouter.delete("/:id", deletePost);
 
 export default postRouter;
