40b31712f9dedad9698a414d6c789697af4704a1
petardjorovic <perodjorovic@gmail.com>
2026-03-04 17:18:31 +0100

    implement coddeRabbit suggestions

--- identity-service/tests/unit/middleware/asyncHandler.middleware.test.ts
+++ identity-service/tests/unit/middleware/asyncHandler.middleware.test.ts
@@ -14,6 +14,7 @@ describe("asyncHandler middleware", () => {
     await handler(req, res, next);
 
     expect(controller).toHaveBeenCalledWith(req, res, next);
+    expect(next).not.toHaveBeenCalled();
   });
 
   it("should call next with error if controller throws", async () => {
