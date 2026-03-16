9d314536984e300ec63c23f1db4a8cbe634512ed
petardjorovic <perodjorovic@gmail.com>
2026-03-16 21:37:38 +0100

    add repository layer

---
+++ post-service/src/repositories/post.repository.ts
@@ -0,0 +1,59 @@
+import PostModel, { PostDocument } from "../models/post.model.js";
+
+export const createNewPost = async (
+  userId: string,
+  content: string,
+  mediaIds = [],
+): Promise<PostDocument> => {
+  const newlyCreatedPost = await PostModel.create({
+    user: userId,
+    content,
+    mediaIds,
+  });
+
+  return newlyCreatedPost;
+};
+
+export const getPostsAndPostsCount = async (
+  skip: number,
+  limitPerPage: number,
+): Promise<{ posts: PostDocument[]; totalPosts: number }> => {
+  const [posts, totalPosts] = await Promise.all([
+    PostModel.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limitPerPage)
+      .lean(),
+    PostModel.countDocuments(),
+  ]);
+
+  return { posts, totalPosts };
+};
+
+export const findPostById = async (
+  postId: string,
+): Promise<PostDocument | null> => {
+  const post = await PostModel.findById(postId);
+  return post;
+};
+
+export const updatePost = async (
+  postId: string,
+  userId: string,
+  updateData: Record<string, any>,
+) => {
+  const updatedPost = await PostModel.findOneAndUpdate(
+    { _id: postId, user: userId },
+    updateData,
+    { new: true },
+  ).lean();
+
+  return updatePost;
+};
+
+export const findPostAndDelete = async (postId: string, userId: string) => {
+  return await PostModel.findOneAndDelete({
+    _id: postId,
+    user: userId,
+  });
+};
